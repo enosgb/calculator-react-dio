9fc982dca381238edd5eaa387cea9e129d96b374
Enos Gabriel <enosgb55@gmail.com>
2022-11-03 21:10:50 -0300

    commit initial

---
+++ src/components/Button/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { ButtonContainer } from "./styles";
+export default function Button({ label, onClick }) {
+  return <ButtonContainer onClick={onClick}>{label}</ButtonContainer>;
+}
